a647a596f19b72aba98d913ba35b823ab3311eba
Jacob Gee-Clarke <jacob.clarke@gmail.com>
2018-12-26 23:53:08 +0000

    Added cleanup command to remove all node_modules folders

--- yarn-recursive.js
+++ yarn-recursive.js
@@ -6,47 +6,53 @@ const argv = require('yargs').argv;
 const clc = require('cli-color');
 
 function packageJsonLocations(dirname) {
-  return shell.find(dirname)
-    .filter(fname => !(fname.indexOf('node_modules') > -1 || fname[0] === '.') && path.basename(fname) === 'package.json')
-    .map(fname => path.dirname(fname));
+    return shell.find(dirname)
+        .filter(fname => !(fname.indexOf('node_modules') > -1 || fname[0] === '.') && path.basename(fname) === 'package.json')
+        .map(fname => path.dirname(fname));
 }
 
 function yarn(directoryName) {
-  let command = 'yarn';
+    let command = 'yarn';
 
-  if (argv.cmd)
-    command += ' ' + argv.cmd;
+    if (argv.cmd) {
+        command += ' ' + argv.cmd;
+    }
 
-  if (argv.opt)
-    command += ' ' + argv.opt;
+    if (argv.opt)
+        command += ' ' + argv.opt;
 
-  console.log(clc.blueBright('Current yarn path: ' + directoryName + '/package.json...'));
- 
-  shell.cd(directoryName);  
-  let result = shell.exec(command);
+    console.log(clc.blueBright('Current yarn path: ' + directoryName + '/package.json...'));
+    let result;
+    if (argv.cmd === "cleanup") {
+        shell.cd(directoryName);
+        result = shell.rm('-r', 'node_modules');
+    } else {
+        shell.cd(directoryName);
+        result = shell.exec(command);
+    }
 
-  return {
-    directoryName: directoryName,
-    exitCode: result.code
-  };
+    return {
+        directoryName: directoryName,
+        exitCode: result.code
+    };
 }
 
 function filterRoot(directoryName) {
-  console.log('Root filtering');
+    console.log('Root filtering');
 
-  return path.normalize(directoryName) === path.normalize(process.cwd());
+    return path.normalize(directoryName) === path.normalize(process.cwd());
 }
 
 if (require.main === module) {
-  let exitCode = packageJsonLocations(process.cwd())
-    .filter(argv.skipRoot ? filterRoot : filtered => filtered)
-    .map(yarn)
-    .reduce((code, result) =>result.exitCode > code ? result.exitCode : code, 0);
+    let exitCode = packageJsonLocations(process.cwd())
+        .filter(argv.skipRoot ? filterRoot : filtered => filtered)
+        .map(yarn)
+        .reduce((code, result) => result.exitCode > code ? result.exitCode : code, 0);
 
-  console.log(clc.green('End of yarns'));
-  process.exit(exitCode);
+    console.log(clc.green('End of yarns'));
+    process.exit(exitCode);
 }
 
 module.exports = {
-  yarn: yarn
+    yarn: yarn
 };
